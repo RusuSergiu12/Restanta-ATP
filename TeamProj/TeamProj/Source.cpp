e345d6b9681b0de0347357cb2ed60bc8bf4cfbf8
RusuSergiu12 <rususergiu21@stud.ase.ro>
2022-11-27 23:39:09 +0200

    cleared source.cpp

--- TeamProj/TeamProj/Source.cpp
+++ TeamProj/TeamProj/Source.cpp
@@ -2,8 +2,5 @@
 using namespace std;
 
 int main() {
-	cout << "Hello World!!" << endl;
-	cout << " Rob " << endl;
-	cout << "Roxana aici" << endl;
-	cout << "Sergiu e si el aici" << endl;
+	// clear
 }
